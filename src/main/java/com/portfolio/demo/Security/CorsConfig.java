ca009e98884a47cc25cb44a836ff7b3bf9e737e2
CPaulo18 <paulo.ramon.caceres@gmail.com>
2023-06-30 01:32:42 -0300

    actualizado cors

--- src/main/java/com/portfolio/demo/Security/CorsConfig.java
+++ src/main/java/com/portfolio/demo/Security/CorsConfig.java
@@ -14,12 +14,12 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/login")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("https://frontendap-65c03.web.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .exposedHeaders("*");
 
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("https://frontendap-65c03.web.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
         };
